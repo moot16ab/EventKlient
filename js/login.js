eda5c74a778445dd8ff6452e00a397d592d99810
moot16ab <ottesen.morten97@gmail.com>
2017-12-21 04:45:15 +0100

    tilføjet SDK og fået login til at virke

---
+++ js/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#log-ind-knap").on("click", function(){
+        var email = $("#inputEmail").val();
+        var password = $("#inputPassword").val();
+
+        SDK.login(email, password, function (err, data) {
+            if(err){
+                alert(err)
+            } else{
+                window.location.href = "events.html";
+            }
+        })
+    })
+});
---
+++ js/sdk.js
@@ -0,0 +1,14 @@
+const SDK = {
+    login: function(email, password, callback){
+        //hardcoded test bruger
+        if(email ===  "mote12@cbs.dk" && password === "Haps1234"){
+            var data = {idStudent: 67, firstName: "Morten", lastName: "Ottesen", email: "mote12@cbs.dk"};
+            sessionStorage.setItem("token", email + "-token");
+            sessionStorage.setItem("bruger", JSON.stringify(data));
+            callback(null, data)
+        } else{
+            var err = "Ugyldigt login";
+            callback(err)
+        }
+    }
+}
