b6c1fd4d97af7761c27fbfb65ca15b01996e47b5
moot16ab <ottesen.morten97@gmail.com>
2017-12-21 09:47:40 +0100

    Nu kan man se en list over alle events, som man selv deltager i

--- js/sdk.js
+++ js/sdk.js
@@ -5,7 +5,7 @@ const SDK = {
             if(fornavn && efternavn && email && password){
                 var id = new Date().getTime();
                 sessionStorage.setItem("token", email + "-token");
-                sessionStorage.setItem("bruger", JSON.stringify({idStudent: id, firstName: fornavn, lastName: efternavn, email: email, password: password}));
+                sessionStorage.setItem("bruger", JSON.stringify({idStudent: id, firstName: fornavn, lastName: efternavn, email: email, password: password, deltager: []}));
                 callback(null, data)
             } else{
                 var err = "Oprettelsen mislykkedes"
@@ -42,6 +42,11 @@ const SDK = {
             callback(null, data.events)
         },
 
+        hentTilmeldteEvents: function (callback){
+            var bruger = JSON.parse(sessionStorage.getItem('bruger'));
+            callback(null, bruger.deltager)
+        },
+
         deltag: function (idEvent) {
             var valgteEvent = null;
             function isId(event) {
@@ -51,7 +56,6 @@ const SDK = {
             data.events.find(isId);
             var bruger = JSON.parse(sessionStorage.getItem('bruger'));
             valgteEvent.deltagere.push(bruger)
-            console.log(valgteEvent.deltagere)
             alert("Du er blevet tilmeldt \"" + valgteEvent.eventName + "\"");
         },
 
@@ -75,10 +79,10 @@ const SDK = {
 
 var data = {
     brugere: [
-        {idStudent: 67, firstName: "Morten", lastName: "Ottesen", email: "mote12@cbs.dk", password: "Haps1234"},
-        {idStudent: 92, firstName: "Frank", lastName: "Hvam", email: "fh@gmail.com", password: "frank"},
-        {idStudent: 30, firstName: "Alberto", lastName: "Contador", email: "ac@gmail.com", password: "alberto"},
-        {idStudent: 56, firstName: "Morten", lastName: "Breum", email: "mb@gmail.com", password: "morten"}
+        {idStudent: 67, firstName: "Morten", lastName: "Ottesen", email: "mote12@cbs.dk", password: "Haps1234", deltager: [{idEvent: 63, eventName: "DØK Hyttetur", owner: "67", location: "Frederikberg", price: 250, eventDate: "10-04-2018", description: "Vi glæder os til at se jer", isDeletede: 0},{idEvent: 64, eventName: "DØK Ski", owner: "67", location: "Val T", price: 4000, eventDate: "04-12-2018", description: "FEST I Val T", isDeletede: 0}, {idEvent: 70, eventName: "Julebord", owner: "30", location: "Østerbrohallen", price: 230, eventDate: "09-12-1997", description: "Julefrokost. Fri bar", isDeletede: 0}]},
+        {idStudent: 92, firstName: "Frank", lastName: "Hvam", email: "fh@gmail.com", password: "frank", deltager: [{idEvent: 63, eventName: "DØK Hyttetur", owner: "67", location: "Frederikberg", price: 250, eventDate: "10-04-2018", description: "Vi glæder os til at se jer", isDeletede: 0},{idEvent: 64, eventName: "DØK Ski", owner: "67", location: "Val T", price: 4000, eventDate: "04-12-2018", description: "FEST I Val T", isDeletede: 0}, {idEvent: 66, eventName: "Torsdagsfest", owner: "92", location: "Nexus", price: 35, eventDate: "10-04-2017", description: "Det blir fest", isDeletede: 0},{idEvent: 68, eventName: "DØK Jul", owner: "30", location: "Bit-Lab", price: 350, eventDate: "24-12-2017", description: "DØK Jul for de mest inkarnerede DØK'ere", isDeletede: 0}]},
+        {idStudent: 30, firstName: "Alberto", lastName: "Contador", email: "ac@gmail.com", password: "alberto", deltager: [{idEvent: 63, eventName: "DØK Hyttetur", owner: "67", location: "Frederikberg", price: 250, eventDate: "10-04-2018", description: "Vi glæder os til at se jer", isDeletede: 0},{idEvent: 68, eventName: "DØK Jul", owner: "30", location: "Bit-Lab", price: 350, eventDate: "24-12-2017", description: "DØK Jul for de mest inkarnerede DØK'ere", isDeletede: 0},{idEvent: 70, eventName: "Julebord", owner: "30", location: "Østerbrohallen", price: 230, eventDate: "09-12-1997", description: "Julefrokost. Fri bar", isDeletede: 0}]},
+        {idStudent: 56, firstName: "Morten", lastName: "Breum", email: "mb@gmail.com", password: "morten", deltager: [{idEvent: 63, eventName: "DØK Hyttetur", owner: "67", location: "Frederikberg", price: 250, eventDate: "10-04-2018", description: "Vi glæder os til at se jer", isDeletede: 0},{idEvent: 64, eventName: "DØK Ski", owner: "67", location: "Val T", price: 4000, eventDate: "04-12-2018", description: "FEST I Val T", isDeletede: 0},{idEvent: 66, eventName: "Torsdagsfest", owner: "92", location: "Nexus", price: 35, eventDate: "10-04-2017", description: "Det blir fest", isDeletede: 0},{idEvent: 68, eventName: "DØK Jul", owner: "30", location: "Bit-Lab", price: 350, eventDate: "24-12-2017", description: "DØK Jul for de mest inkarnerede DØK'ere", isDeletede: 0},{idEvent: 70, eventName: "Julebord", owner: "30", location: "Østerbrohallen", price: 230, eventDate: "09-12-1997", description: "Julefrokost. Fri bar", isDeletede: 0},{idEvent: 71, eventName: "Makroøkonomi", owner: "56", location: "sps16", price: 0, eventDate: "29-11-2017", description: "Dette er så sjovt", isDeletede: 0}]}
     ],
 
     events: [
@@ -86,7 +90,7 @@ var data = {
         {idEvent: 64, eventName: "DØK Ski", owner: "67", location: "Val T", price: 4000, eventDate: "04-12-2018", description: "FEST I Val T", isDeletede: 0, deltagere: [{idStudent: 67, firstName: "Morten", lastName: "Ottesen", email: "mote12@cbs.dk", password: "Haps1234"},{idStudent: 92, firstName: "Frank", lastName: "Hvam", email: "fh@gmail.com", password: "frank"},{idStudent: 56, firstName: "Morten", lastName: "Breum", email: "mb@gmail.com", password: "morten"}]},
         {idEvent: 66, eventName: "Torsdagsfest", owner: "92", location: "Nexus", price: 35, eventDate: "10-04-2017", description: "Det blir fest", isDeletede: 0, deltagere: [{idStudent: 92, firstName: "Frank", lastName: "Hvam", email: "fh@gmail.com", password: "frank"},{idStudent: 56, firstName: "Morten", lastName: "Breum", email: "mb@gmail.com", password: "morten"}]},
         {idEvent: 68, eventName: "DØK Jul", owner: "30", location: "Bit-Lab", price: 350, eventDate: "24-12-2017", description: "DØK Jul for de mest inkarnerede DØK'ere", isDeletede: 0, deltagere: [{idStudent: 92, firstName: "Frank", lastName: "Hvam", email: "fh@gmail.com", password: "frank"},{idStudent: 30, firstName: "Alberto", lastName: "Contador", email: "ac@gmail.com", password: "alberto"},{idStudent: 56, firstName: "Morten", lastName: "Breum", email: "mb@gmail.com", password: "morten"}]},
-        {idEvent: 70, eventName: "Julebord", owner: "30", location: "Østerbrohallen", price: 230, eventDate: "09-12-1997", description: "Julefrokost. Fri bar", isDeletede: 0, deltagere: [{idStudent: 30, firstName: "Alberto", lastName: "Contador", email: "ac@gmail.com", password: "alberto"},{idStudent: 56, firstName: "Morten", lastName: "Breum", email: "mb@gmail.com", password: "morten"}]},
+        {idEvent: 70, eventName: "Julebord", owner: "30", location: "Østerbrohallen", price: 230, eventDate: "09-12-1997", description: "Julefrokost. Fri bar", isDeletede: 0, deltagere: [{idStudent: 30, firstName: "Alberto", lastName: "Contador", email: "ac@gmail.com", password: "alberto"},{idStudent: 56, firstName: "Morten", lastName: "Breum", email: "mb@gmail.com", password: "morten"},{idStudent: 67, firstName: "Morten", lastName: "Ottesen", email: "mote12@cbs.dk", password: "Haps1234"}]},
         {idEvent: 71, eventName: "Makroøkonomi", owner: "56", location: "sps16", price: 0, eventDate: "29-11-2017", description: "Dette er så sjovt", isDeletede: 0, deltagere: [{idStudent: 56, firstName: "Morten", lastName: "Breum", email: "mb@gmail.com", password: "morten"}]}
     ]
 }
---
+++ js/tilmeldinger.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    SDK.Event.hentTilmeldteEvents(function(err, events){
+        if(err) console.log(err);
+        var $eventTable = $('#eventTable');
+
+        events.forEach(function (event) {
+
+            $eventTable.append(
+                "<tr>" +
+                "<td>" + event.eventName + "</td>" +
+                "<td>" + event.location + "</td>" +
+                "<td>" + event.price + "</td>" +
+                "<td>" + event.eventDate + "</td>" +
+                "<td>" + event.description + "</td>" +
+                "<td><button class='btn btn-success deltag-knap' data-event='" + event.idEvent + "'>Deltag</button></td>" +
+                "<td><button class='btn btn-primary deltagere-knap' data-event='" + event.idEvent + "'>Vis deltagere</button></td>" +
+                "</tr>");
+        });
+
+        $(".deltag-knap").click(function () {
+            var e = this;
+            SDK.Event.deltag(e.dataset.event)
+        })
+
+        $(".deltagere-knap").click(function () {
+            var e = this;
+            SDK.Event.visDeltagere(e.dataset.event)
+        })
+
+
+    });
+})
